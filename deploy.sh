89647c1ca2c38b22ec0b553db74db37451f03b35
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-01-20 02:16:22 +0100

    Use also bower for angular dependency.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 rm -Rf app/lib/*
 mkdir app/lib/
+cp bower_components/angularjs/angular.min.js app/lib/
+cp bower_components/angularjs/angular-route.min.js app/lib/
+cp bower_components/angularjs/angular-touch.min.js app/lib/
 cp bower_components/modernizr/modernizr.js app/lib/
 cp bower_components/shake.js/shake.js app/lib/
 
